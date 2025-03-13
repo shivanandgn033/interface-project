bf2ac587c337c1ec80bb3dc9a34ef9a020021fc1
shivanandgn033 <shivanandgn033@gmail.com>
2025-03-13 13:47:57 +0530

    Build run

--- obj/Debug/net9.0/interface-project.AssemblyInfo.cs
+++ obj/Debug/net9.0/interface-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("interface-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3edb3688c74feecfc921a10f1052c9305dd9cde5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042140ba668ed91390d98395b402a80337aa1da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("interface-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("interface-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
